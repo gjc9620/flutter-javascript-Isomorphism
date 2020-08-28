58371527c3891feb5319cf32ab4bcf23445f34c6
gujiacheng07691 <gujiacheng07691@hellobike.com>
2020-08-28 23:13:01 +0800

    ok

--- dart2js.js
+++ dart2js.js
@@ -1,6 +1,10 @@
+'use strict';
+const dart_sdk = require('dart_sdk');
+const core = dart_sdk.core;
+const js = dart_sdk.js;
+const dart = dart_sdk.dart;
+const dartx = dart_sdk.dartx;
 var dart2js = Object.create(dart.library);
-export { dart2js };
-import { core, js, dart, dartx } from 'dart_sdk.js';
 var StringL = () => (StringL = dart.constFn(dart.legacy(core.String)))();
 var VoidToStringL = () => (VoidToStringL = dart.constFn(dart.fnType(StringL(), [])))();
 var FunctionL = () => (FunctionL = dart.constFn(dart.legacy(core.Function)))();
@@ -16,5 +20,7 @@ dart.trackLibraries("dart2js", {
   "org-dartlang-app:/dart2js.dart": dart2js
 }, {
 }, null);
+// Exports:
+exports.dart2js = dart2js;
 
 //# sourceMappingURL=dart2js.js.map
