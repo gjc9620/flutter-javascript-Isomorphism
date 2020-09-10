0cfb68617c5f47cde2636d92bb0d702ebc213e9d
gujiacheng07691 <gujiacheng07691@hellobike.com>
2020-09-10 15:38:39 +0800

    flutter

--- d2js.js
+++ d2js.js
@@ -1,17 +1,15 @@
 const {dart2js} = require('./dart2js.js');
 
 
-const getValue = (fun, arg = {})=>{
+const callDart = (fun, arg = {})=>{
   const resStr = fun(JSON.stringify(arg));
   const res = JSON.parse(resStr)
   const v = res.data;
   console.log(v);
   return v
-}
+} 
 
-getValue(dart2js.foo, { n:1, m:2 })
-getValue(dart2js.foo2)
-getValue(dart2js.changeList, { list: [{ name: 'Tom', count: 10 }, { name: 'jack', count: 20 }] })
+
 
 
 
